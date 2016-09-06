d0e202fb64bc45564ea49716796d5638643b2db1
Sam Chrisinger <s.chrisinger@gmail.com>
2016-09-06 09:24:15 -0400

    First commit

---
+++ src/widget.js
@@ -0,0 +1,48 @@
+class SpinitronWidget {
+    constructor(
+        elementId,
+        station,
+        numTracks,
+        reloadInterval=5,
+        showTwitterLinks=false,
+        showTimestamps=false,
+        showLinks=true
+    ) {
+        this.element = document.getElementById(elementId);
+        this.attrs = {
+            station: station,
+            num: numTracks,
+            time: showTimestamps ? 1 : 0,
+            nolinks: showLinks ? 0 : 1,
+            tweets: showTwitterLinks ? 1 : 0
+        };
+
+        this.reload();
+        window.setInterval(() => this.reload(), reloadInterval * 1000);
+    }
+
+    buildQuery() {
+        let query = Object.keys(this.attrs).map((key) => {
+            return encodeURIComponent(key) + '=' + encodeURIComponent(this.attrs[key]);
+        });
+
+        var fn = '_spinitron' + (Math.random().toString() + new Date().getTime()).slice(2, -1);
+        window[fn] = (html) => {
+            this.element.innerHTML = html;
+            this.script.parentElement.removeChild(this.script);
+            delete window[fn];
+        };
+        query.push('callback=' + fn);        
+        return query;
+        
+    }
+
+    reload() {
+        this.script = document.createElement('script');
+        var query = this.buildQuery();
+        this.script.src = '//spinitron.com/radio/newestsong.php?' + query.join('&');
+        document.getElementsByTagName('head')[0].appendChild(this.script);
+    }
+}
+
+export default SpinitronWidget;
